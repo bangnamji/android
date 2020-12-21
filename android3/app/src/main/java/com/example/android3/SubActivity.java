ccbf4463606e75598aab096ca0e82be26989b81a
namji <namjibhang@gmail.com>
2020-12-21 23:46:28 +0900

    12월 21일 커밋

---
+++ android3/app/src/main/java/com/example/android3/SubActivity.java
@@ -0,0 +1,15 @@
+package com.example.android3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
+
