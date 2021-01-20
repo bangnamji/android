eb141c922116c7a1f65a491c068767774dadb8fa
namji <namjibhang@gmail.com>
2021-01-21 03:09:53 +0900

    addview 수정중

--- DiaryinMyHand/app/src/main/java/com/example/diaryinmyhand/MainActivity2.kt
+++ DiaryinMyHand/app/src/main/java/com/example/diaryinmyhand/MainActivity2.kt
@@ -4,6 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.diaryinmyhand.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -13,6 +19,31 @@ class MainActivity2 : AppCompatActivity() {
         binding = ActivityMain2Binding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        var linearLayout: LinearLayout = findViewById(R.id.list)
+        for (i in 1 .. 10) {
+            val view: View = layoutInflater.inflate(R.layout.diaryitem,null)
+            val tvItem: TextView = view.findViewById(R.id.item_title)
+            tvItem.setText(""+ i +"번째 다이어리")
+            linearLayout.addView(view)
+        }
+
+
+//        val DiaryList = ArrayList<DiaryForList>()
+//        for(i in 0 until 10) {
+//            DiaryList.add(DiaryForList(""+ i +"번째 일기"))
+//        }
+//
+//        val container = binding.list
+//        val inflater = LayoutInflater.from(this@MainActivity2)
+//
+//        for(i in 0 until DiaryList.size) {
+//            val itemView = inflater.inflate(R.layout.diaryitem, null)
+//            val diaryTime = itemView.findViewById<TextView>(R.id.item_title)
+//            diaryTime.text = DiaryList.get(i).time
+//            container.addView(itemView)
+//        }
+
 
         binding.Settings.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
@@ -31,3 +62,7 @@ class MainActivity2 : AppCompatActivity() {
 
     }
 }
+
+//class DiaryForList(val time:String) {
+//
+//}
